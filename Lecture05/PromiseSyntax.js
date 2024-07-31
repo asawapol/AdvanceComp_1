c41b0034a6c3a89a576797a7f18d2f934ebf1162
asawapol <138425079+asawapol@users.noreply.github.com>
2024-07-31 09:29:49 +0700

    week5

---
+++ Lecture05/PromiseSyntax.js
@@ -0,0 +1,11 @@
+let myPromise = new Promise(function(myResolve, myReject) {
+
+    myResolve();
+    myReject();
+})
+
+
+myPromise.then(
+    function(value){},
+    function(error){}
+);
---
+++ Lecture05/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+// async function longRunningOperation(){
+// //     //logic
+// //     return 42;
+// // }
